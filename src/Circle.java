0f5fb1e3175c931e85679197b43f0edc8fee4cf0
dani3700 <dani3700@ISS147D8-12.scdsb.pri>
2014-11-07 09:15:07 -0500

    initial commit

---
+++ src/Circle.java
@@ -0,0 +1,26 @@
+
+import TurtleGraphics.Pen;
+
+public class Circle implements Shape {
+
+ 
+    public double area() {
+     return 0; 
+    }
+
+    @Override
+    public void draw(Pen p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getXPos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getYPos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/Shape.java
@@ -0,0 +1,11 @@
+import TurtleGraphics.*;
+
+
+
+
+public interface Shape {
+public double area();    
+public void draw(Pen p);
+public double getXPos();
+public double getYPos();
+}
